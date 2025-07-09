cc662e4e91583fcc50884a2c9d3c2a620bb6573b
SaiTeja Amshala <samshala@ttu.edu>
2025-07-09 10:05:50 -0500

    Day-08 Solved Leetcode LC 200 Day-09 Solved Leetcode LC 200

---
+++ Day 9/LC 200.py
@@ -0,0 +1,55 @@
+# 200. Number of Islands
+
+# Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+# An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. 
+# You may assume all four edges of the grid are all surrounded by water.
+
+ 
+
+# Example 1:
+
+# Input: grid = [
+#   ["1","1","1","1","0"],
+#   ["1","1","0","1","0"],
+#   ["1","1","0","0","0"],
+#   ["0","0","0","0","0"]
+# ]
+# Output: 1
+# Example 2:
+
+# Input: grid = [
+#   ["1","1","0","0","0"],
+#   ["1","1","0","0","0"],
+#   ["0","0","1","0","0"],
+#   ["0","0","0","1","1"]
+# ]
+# Output: 3
+
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        if not grid:
+            return 0
+
+        def dfs(i,j):
+            if i <0 or i >= m or j < 0 or j >= n or grid[i][j] != '1':
+                return 
+            else:
+                grid[i][j] = '0'
+                dfs(i+1, j)
+                dfs(i-1, j)
+                dfs(i, j+1)
+                dfs(i, j-1)
+
+        m, n = len(grid), len(grid[0])
+        num_islands = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    num_islands += 1
+                    dfs(i, j)
+        return num_islands
